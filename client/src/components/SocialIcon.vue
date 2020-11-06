4dadbfab16cc9de1e5c0359b5475665c0bbba702
stefan-d-b <stefan@esser-emmerik.nl>
2020-11-06 22:16:10 +0100

    add home page from 2

---
+++ client/src/components/SocialIcon.vue
@@ -0,0 +1,13 @@
+<template>
+  <a :href="url">
+    <font-awesome-icon :icon="icon"    size="5x"/>
+    <!-- v-tooltip.bottom="tooltip"   -->
+  </a>
+</template>
+
+<script>
+export default {
+  name: "SocialIcon",
+  props: ["url", "icon", "tooltip"]
+};
+</script>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,18 +1,36 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div
+    class="absolute inset-0 w-full h-screen bg-cover bg-center bg-blue-300"
+    :style="{}"
+  >
+  <!-- backgroundImage: 'url(' + require('../assets/images/backgrounds/background-1.jpg') + ')' -->
+    <div class="w-full h-screen flex justify-center content-center flex-wrap">
+      <div class="w-full flex justify-center">
+        <div class="text-black px-8 rounded-lg text-center border-2">
+          <span class="font-bold text-4xl">Stefan de Bruin</span>
+          <hr />
+          <span class="font-thin text-2xl">Full stack developer</span>
+        </div>
+      </div>
+
+      <div class="w-full flex justify-center mt-5">
+        <social-icon class="text-black border-2 border-white rounded-full p-3" :icon="['fab', 'github']" url="https://github.com/stefandb"></social-icon>
+
+        <social-icon class="text-black border-2 border-white rounded-full p-3 ml-5 mr-5" :icon="['fab', 'bitbucket']" url="https://bitbucket.org/%7Baba10776-c6d7-427a-a898-616c63629386%7D/"></social-icon>
+
+        <social-icon class="text-black border-2 border-white rounded-full p-3" :icon="['fab', 'twitter']" url="https://twitter.com/stefan_d_b"></social-icon>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import SocialIcon from "@/components/SocialIcon.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
+    SocialIcon
   }
-}
+};
 </script>
