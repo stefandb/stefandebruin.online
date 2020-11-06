c4558dc089a53682834faa0dad205870fcb355a3
stefan-d-b <stefan@esser-emmerik.nl>
2020-11-06 21:21:07 +0100

    init tailwindcss

--- client/src/main.js
+++ client/src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 import router from './router'
 // import store from './store'
 
+import '@/assets/css/app.css'
+
+
 // createApp(App).use(store).use(router).mount('#app')
 
 createApp(App).use(router).mount('#app')
