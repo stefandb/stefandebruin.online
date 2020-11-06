fa3b417897c6135b305e845460c9a89261947185
stefan-d-b <stefan@esser-emmerik.nl>
2020-11-06 22:15:55 +0100

    add fontawesome

--- client/src/main.js
+++ client/src/main.js
@@ -5,7 +5,21 @@ import router from './router'
 
 import '@/assets/css/app.css'
 
-
 // createApp(App).use(store).use(router).mount('#app')
 
-createApp(App).use(router).mount('#app')
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faCog,
+  faSearch,
+} from "@fortawesome/free-solid-svg-icons";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add(
+  faCog,
+  faSearch,
+  fab
+);
+
+createApp(App).component("font-awesome-icon", FontAwesomeIcon).use(router).mount('#app')
