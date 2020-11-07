c6c42eb27c1b64d75e9ce56ccae83807cbfb715e
stefan-d-b <stefan@esser-emmerik.nl>
2020-11-07 20:17:43 +0100

    Add outside click directive

--- client/src/main.js
+++ client/src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 // import store from './store'
 
 import '@/assets/css/app.css'
+import VTooltip from 'v-tooltip'
 
 // createApp(App).use(store).use(router).mount('#app')
 
@@ -22,4 +23,35 @@ library.add(
   fab
 );
 
-createApp(App).component("font-awesome-icon", FontAwesomeIcon).use(router).mount('#app')
+
+const clickOutside = {
+  // When the bound element is mounted into the DOM...
+  mounted(el, binding, vnode, prevVnode) {
+    console.log(vnode.component);
+    console.log(prevVnode);
+
+    const handleOutsideClick = (e) => {
+      e.stopPropagation()
+      const { handler, exclude } = binding.value
+      let clickedOnExcludedEl = false
+      exclude.forEach(refName => {
+        if (!clickedOnExcludedEl) {
+          const excludedEl = document.getElementById(refName);
+          clickedOnExcludedEl = excludedEl.contains(e.target)
+        }
+      })
+      if (!el.contains(e.target) && !clickedOnExcludedEl) {
+        handler()
+      }
+    }
+
+    el.__vueClickOutside__ = handleOutsideClick
+    document.addEventListener('click', handleOutsideClick)
+  },
+  unmounted(el) {
+    document.removeEventListener('click', el.__vueClickOutside__);
+    el.__vueClickOutside__ = null;
+  }
+};
+
+createApp(App).component("font-awesome-icon", FontAwesomeIcon).use(router).use(VTooltip).directive('click-outside', clickOutside).mount('#app')
