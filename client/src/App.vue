b19132026b7083e01fc69e3fc2568b866480880f
stefan-d-b <stefan@esser-emmerik.nl>
2020-11-08 20:26:24 +0100

    disable predefined css

--- client/src/App.vue
+++ client/src/App.vue
@@ -19,7 +19,7 @@ export default {
 
 <style>
 
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
@@ -38,5 +38,5 @@ export default {
 
 #nav a.router-link-exact-active {
   color: #42b983;
-}
+} */
 </style>
