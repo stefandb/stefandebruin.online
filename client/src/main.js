5867975b573c5559a0b1566a58cd2cd7be5a3351
stefan-d-b <stefan@esser-emmerik.nl>
2020-11-08 15:30:56 +0100

    fix font awesome icon packs

--- client/src/main.js
+++ client/src/main.js
@@ -10,17 +10,19 @@ import VTooltip from 'v-tooltip'
 
 import { library } from "@fortawesome/fontawesome-svg-core";
 import {
-  faCog,
-  faSearch,
+  // faCog,
+  // faSearch,
+  fas
 } from "@fortawesome/free-solid-svg-icons";
 import { fab } from "@fortawesome/free-brands-svg-icons";
 
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 library.add(
-  faCog,
-  faSearch,
-  fab
+  // faCog,
+  // faSearch,
+  fab,
+  fas
 );
 
 
