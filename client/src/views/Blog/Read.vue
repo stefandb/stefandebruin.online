b845fcaf0be2a23b0863c17bfa93212aa6efa7de
stefan-d-b <stefan@esser-emmerik.nl>
2020-11-22 11:55:38 +0100

    blog post loading

--- client/src/views/Blog/Read.vue
+++ client/src/views/Blog/Read.vue
@@ -1,43 +1,33 @@
 <template>
-<div class=" bg-white overflow-hidden shadow rounded-lg bg-opacity-75">
-
-<div v-if="loading">Loading...</div>
-
-
-  <div class="px-4 py-5 sm:p-6">
-<div class="text-lg max-w-prose mx-auto mb-6">
-      <p class="text-base text-center leading-6 text-indigo-600 font-semibold tracking-wide uppercase">Introducing</p>
-      <h1 class="mt-2 mb-8 text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10">{{post.title}}</h1>
-
-      
-
+  <div>
+    <div class="bg-white overflow-hidden shadow rounded-lg bg-opacity-75" v-if="loading === false">
+      <div class="px-4 py-5 sm:p-6">
+        <div class="text-lg max-w-prose mx-auto mb-6">
+          <p class="text-base text-center leading-6 text-indigo-600 font-semibold tracking-wide uppercase">Introducing</p>
+          <h1 class="mt-2 mb-8 text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10">{{post.title}}</h1>
+        </div>
+        <div class="prose prose-lg text-gray-500 mx-auto" v-html="post.content">
+        </div>
+      </div>
     </div>
-    <div class="prose prose-lg text-gray-500 mx-auto" v-html="post.content"></div>
-    <!-- <div class="prose prose-lg text-gray-500 mx-auto">
-      <p>Faucibus commodo massa rhoncus, volutpat. <strong>Dignissim</strong> sed <strong>eget risus enim</strong>. Mattis mauris semper sed amet vitae sed turpis id. Id dolor praesent donec est. Odio penatibus risus viverra tellus varius sit neque erat velit. Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. <a href="#">Mattis mauris semper</a> sed amet vitae sed turpis id.</p>
-      <ul>
-        <li>Quis elit egestas venenatis mattis dignissim.</li>
-        <li>Cras cras lobortis vitae vivamus ultricies facilisis tempus.</li>
-        <li>Orci in sit morbi dignissim metus diam arcu pretium.</li>
-      </ul>
-      <p>Quis semper vulputate aliquam venenatis egestas sagittis quisque orci. Donec commodo sit viverra aliquam porttitor ultrices gravida eu. Tincidunt leo, elementum mattis elementum ut nisl, justo, amet, mattis. Nunc purus, diam commodo tincidunt turpis. Amet, duis sed elit interdum dignissim.</p>
-      <h2>From beginner to expert in 30 days</h2>
-      <p>Id orci tellus laoreet id ac. Dolor, aenean leo, ac etiam consequat in. Convallis arcu ipsum urna nibh. Pharetra, euismod vitae interdum mauris enim, consequat vulputate nibh. Maecenas pellentesque id sed tellus mauris, ultrices mauris. Tincidunt enim cursus ridiculus mi. Pellentesque nam sed nullam sed diam turpis ipsum eu a sed convallis diam.</p>
-      <blockquote>
-        <p>Sagittis scelerisque nulla cursus in enim consectetur quam. Dictum urna sed consectetur neque tristique pellentesque. Blandit amet, sed aenean erat arcu morbi.</p>
-      </blockquote>
-      <p>Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. Mattis mauris semper sed amet vitae sed turpis id. Id dolor praesent donec est. Odio penatibus risus viverra tellus varius sit neque erat velit.</p>
-      <figure>
-        <img class="w-full rounded-lg" src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&auto=format&fit=facearea&w=1310&h=873&q=80&facepad=3" alt="" width="1310" height="873">
-        <figcaption>Sagittis scelerisque nulla cursus in enim consectetur quam.</figcaption>
-      </figure>
-      <h3>Everything you need to get up and running</h3>
-      <p>Purus morbi dignissim senectus mattis <a href="#">adipiscing</a>. Amet, massa quam varius orci dapibus volutpat cras. In amet eu ridiculus leo sodales cursus tristique. Tincidunt sed tempus ut viverra ridiculus non molestie. Gravida quis fringilla amet eget dui tempor dignissim. Facilisis auctor venenatis varius nunc, congue erat ac. Cras fermentum convallis quam.</p>
-      <p>Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. Mattis mauris semper sed amet vitae sed turpis id. Id dolor praesent donec est. Odio penatibus risus viverra tellus varius sit neque erat velit.</p>
-    </div> -->
-
+    
+    <div class="bg-white overflow-hidden shadow rounded-lg bg-opacity-75" v-if="loading">
+      <div class="px-4 py-5 sm:p-6 animate-pulse">
+        <div class="text-lg max-w-prose mx-auto mb-6">
+          <p class="text-base text-center leading-6 text-indigo-600 font-semibold tracking-wide uppercase">Introducing</p>
+          <h1 class="mt-2 mb-8 text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10">
+            <div class="h-4 bg-blue-400 rounded w-3/6 mx-auto"></div>
+            </h1>
+        </div>
+        <div class="prose prose-lg text-gray-500 mx-auto space-y-2">
+          <div class="h-4 bg-blue-400 rounded "></div>
+          <div class="h-4 bg-blue-400 rounded "></div>
+          <div class="h-4 bg-blue-400 rounded "></div>
+        </div>
+      </div>
+    </div>
   </div>
-</div>
+  
 </template>
 
 
@@ -54,32 +44,37 @@ setup () {
 
     const route = useRoute()
 
-    const { result, loading, error } = useQuery(
-      gql`query blogPost($slug: String!){
-  getObject(bucket_slug: "stefandebruin-portfolio", input: {
-    slug: $slug,
-    read_key: "Q6ghfLG2ggzTfLRUeircNrZ0jAlP87ccRZh0qUu7jIOIAvYcGP"
-  }) {
-    title
-    content
-    metadata
-  }
-}`,
-      {
-        "slug": route.params.slug
-      }
-    );
+    const loading = true;
+//     const { result, loading, error } = useQuery(
+//       gql`query blogPost($bucketSlug: String!, $readKey: String!, $slug: String!){
+//   getObject(bucket_slug: $bucketSlug, input: {
+//     slug: $slug,
+//     read_key: $readKey
+//   }) {
+//     title
+//     content
+//     metadata
+//   }
+// }`,
+//       {
+//         "slug": route.params.slug,
+//         'bucketSlug': process.env.VUE_APP_COSMICJS_BUCKET,
+//         'readKey': process.env.VUE_APP_COSMICJS_READ_KEY,
+//       }
+//     );
 
 
-    onError(error => {
-      console.log('error found 2');
-})
-const post = useResult(result, null, data => data.getObject);
-    return {
-      post,
-      loading,
-      error
-    }
+//     onError(error => {
+//       console.log('error found 2');
+// })
+// const post = useResult(result, null, data => data.getObject);
+//     return {
+//       post,
+//       loading,
+//       error
+//     }
+
+return {loading};
 
   },
 
