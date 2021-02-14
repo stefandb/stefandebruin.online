95d6dbecaa68e97b501654e8e86210164d96b047
stefan-d-b <stefan@esser-emmerik.nl>
2021-02-14 19:41:02 +0100

    pagination update

--- client/src/components/Pagination.vue
+++ client/src/components/Pagination.vue
@@ -1,44 +1,39 @@
 <template>
-    <div class="w-full mt-6 border-t flex items-center border-gray-600 text-white" v-show="totalItems >perPage">
+    <div class="w-full mt-6 border-t flex items-center border-gray-600 text-white select-none" v-show="totalItems > perPage">
         <div class="w-0 flex-1 flex ">
-            <div class="hover:border-green-400 border-transparent border-t-2 pt-4 px-4">
+            <button type="button" class="hover:border-green-400 border-transparent border-t-2 pt-4 px-4 transition ease-in duration-150 hover:scale-125 disabled:scale-100 disabled:transform-none transform disabled:cursor-default disabled-hover:border-transparent active:border-0 focus:border-0 focus:outline-none" :disabled="currentPage === 1" @click="perviousPage">
                 Vorige
-            </div>
+            </button>
         </div>
 
-        <div class="flex">
-            <div class="pt-4 px-4 border-transparent border-t-2 hover:border-green-400" :class="{'border-purple-400 text-purple-400': isActive(1)}">
+        <div class="flex hidden sm:flex">
+            <button type="button" class="hover:border-green-400 border-transparent border-t-2 pt-4 px-4 transition ease-in duration-150 hover:scale-125 disabled:scale-100 disabled:transform-none transform disabled:cursor-default disabled-hover:border-purple-400 active:border-0 focus:border-0 focus:outline-none" :class="{'border-purple-400 text-purple-400': isActive(1)}" @click="goToPage(1)" :disabled="currentPage === 1">
                 1
+            </button>
+            
+            <div class="pt-4 px-4 border-transparent border-t-2" :class="{'border-purple-400 text-purple-400': isActive('d')}" v-if="(pages[0] - 1) !== 1">
+                ...
+            </div>
+            
+
+            <button type="button" v-for="(page, index) in pages" :key="index" class="hover:border-green-400 border-transparent border-t-2 pt-4 px-4 transition ease-in duration-150 hover:scale-125 disabled:scale-100 disabled:transform-none transform disabled:cursor-default disabled-hover:border-purple-400 active:border-0 focus:border-0 focus:outline-none" :class="{'border-purple-400 text-purple-400': isActive(page)}" @click="goToPage(page)" :disabled="currentPage === page">
+                {{ page }}
+            </button>
+
+            <div class="pt-4 px-4 border-transparent border-t-2" :class="{'border-purple-400 text-purple-400': isActive('d')}" v-if="(Number(pages.slice(-1)) + 1) !== lastPage">
+                ...
             </div>
 
-
-            <div class="pt-4 px-4 border-transparent border-t-2 hover:border-green-400">
-                2
-            </div>
-            <div class="pt-4 px-4 border-transparent border-t-2 hover:border-green-400">
-                3
-            </div>
-            <div class="pt-4 px-4 border-transparent border-t-2">
-                {{ currentInMiddle }}
-            </div>
-            <div class="pt-4 px-4 border-transparent border-t-2 hover:border-green-400">
-                7
-            </div>
-            <div class="pt-4 px-4 border-transparent border-t-2 hover:border-green-400">
-                8
-            </div>
-
-
-            <div class="pt-4 px-4 border-transparent border-t-2 hover:border-green-400" :class="{'border-purple-400 text-purple-400': isActive(lastPage)}">
+            <button type="button" class="hover:border-green-400 border-transparent border-t-2 pt-4 px-4 transition ease-in duration-150 hover:scale-125 disabled:scale-100 disabled:transform-none transform disabled:cursor-default disabled-hover:border-purple-400 active:border-0 focus:border-0 focus:outline-none" :class="{'border-purple-400 text-purple-400': isActive(lastPage)}" @click="goToPage(lastPage)" :disabled="currentPage === lastPage">
                 {{ lastPage }}
-            </div>
+            </button>
         </div>
 
 
         <div class="w-0 flex-1 flex justify-end ">
-            <div class="hover:border-green-400 border-transparent border-t-2 pt-4 px-4">
+            <button type="button" class="hover:border-green-400 border-transparent border-t-2 pt-4 px-4 transition ease-in duration-150 hover:scale-125 disabled:scale-100 disabled:transform-none transform disabled:cursor-default disabled-hover:border-transparent active:border-0 focus:border-0 focus:outline-none" :disabled="currentPage === lastPage" @click="nextPage">
                 Volgende
-            </div>
+            </button>
         </div>
     </div>
 </template>
@@ -48,15 +43,63 @@ export default {
     props: ['currentPage', 'totalItems', 'perPage'],
     computed: {
         lastPage () {
-            return this.totalItems / this.perPage;
+            return Math.ceil(this.totalItems / this.perPage);
         },
-        currentInMiddle () {
-            return this.lastPage - this.currentPage > 2 && this.currentPage > 3
-        }
+        pages () {
+            let pages = [];
+            if (this.lastPage <= 7) {
+                for (let i = 2; i < this.lastPage; i++) {
+                    pages.push(i);
+                }
+            } else {
+                let before = 0;
+                let after = 0;
+
+                for (let i = (this.currentPage-1); i > 1; i--) {
+                    if (before === 2) {
+                        break;
+                    }
+                    pages.unshift(i);
+                    before++;
+                }
+                    
+                if(this.currentPage !== 1 && this.currentPage !== this.lastPage){
+                    pages.push(this.currentPage);    
+                }
+
+                for (let i = (this.currentPage+1); i < this.lastPage; i++) {
+                    if (after === (2 + (2 - before) + (this.currentPage === 1 ? 1 : 0))) {
+                        break;
+                    }
+                    pages.push(i);
+                    after++;
+                }
+
+                for (let i = (this.currentPage-1 - before); i > 1; i--) {
+                    if (before === (2 + (2 - after) + (this.currentPage === this.lastPage ? 1 : 0))) {
+                        break;
+                    }
+                    pages.unshift(i);
+                    before++;
+                }
+            }
+
+            return pages;
+        },
+  
     },
     methods: {
         isActive (pageIndex) {
             return pageIndex === this.currentPage;
+        },
+        perviousPage () {
+            this.$emit('new-page-index', this.currentPage - 1);
+        },
+        goToPage (pageIndex) {
+            this.$emit('new-page-index', pageIndex);
+        },
+        nextPage () {
+            this.$emit('new-page-index', this.currentPage + 1);
         }
     }
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,3 +1,5 @@
+const plugin = require('tailwindcss/plugin');
+
 module.exports = {
   future: {
     // removeDeprecatedGapUtilities: true,
@@ -8,8 +10,26 @@ module.exports = {
   theme: {
     extend: {},
   },
-  variants: {},
+  variants: {
+    extend: {
+      scale: ['disabled'],
+      transform: ['disabled'],
+      cursor: ['disabled', 'disabled-hover'],
+      borderWidth: ['disabled-hover', 'active'],
+      borderColor: ['disabled-hover', 'active']
+    }
+  },
   plugins: [
-    require('@tailwindcss/ui'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/aspect-ratio'),
+
+    plugin(function({ addVariant, e }) {
+      addVariant('disabled-hover', ({ modifySelectors, separator }) => {
+        modifySelectors(({ className }) => {
+          return `.${e(`disabled-hover${separator}${className}`)}[disabled]:hover`
+        })
+      })
+    })
   ],
 }
