688cbfb0b83251a126e7c8608f9abd38364667d2
stefan-d-b <stefan@esser-emmerik.nl>
2021-02-23 13:42:55 +0100

    update gql query for pagination

--- client/src/gql/posts.query.js
+++ client/src/gql/posts.query.js
@@ -1,12 +1,13 @@
 import { gql } from "apollo-boost";
 
 export const postsQuery = gql`
-query blogPost($bucketSlug: String!, $readKey: String!){
+query blogPost($bucketSlug: String!, $readKey: String!, $skip: Int!){
   getObjects(bucket_slug: $bucketSlug, input: {
     limit: 8,
+    skip: $skip
     read_key: $readKey,
     type: "posts",
-    sort: published_at_dec,
+    sort: created_at_dec,
     status: published
   }) {
     objects {
@@ -16,7 +17,9 @@ query blogPost($bucketSlug: String!, $readKey: String!){
       metadata
       modified_at
       published_at
-    }
+    },
+    total,
+    limit
   }
 }
 `;
